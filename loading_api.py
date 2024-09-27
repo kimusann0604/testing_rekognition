2f49954d8966333791f28aa16858c62d484891a3
kimuhooo <fantianmucun@gmail.com>
2024-09-28 02:46:41 +0900

    🍭読み込み改善版

--- loading_api.py
+++ loading_api.py
@@ -7,12 +7,17 @@ from boto3 import Session
 config_ini = configparser.ConfigParser()
 config_ini.read('')#config.iniから読み込む
 
-config_key = 'rekognition' #config_iniのrekognitionを呼び出す
 my_region = 'choose_area' #エリアの選択
 
 session = Session(
-    aws_access_key_id=config_key['accsesskey'],
-    aws_secret_access_key=config_key['secretaccesskey'],
+    aws_access_key_id=config_ini['rekognition']['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=config_ini['rekognition']['AWS_SECRET_ACCESS_KEY'],
     region_name=my_region)
+# もしくは
+# config_key = config_ini['rekognition']
+# session = Session(
+#     aws_access_key_id=config_key['AWS_ACCESS_KEY_ID'],
+#     aws_secret_access_key=config_key['AWS_SECRET_ACCESS_KEY'],
+#     region_name=my_region)
 
 client = session.client(service_name='rekognition')
